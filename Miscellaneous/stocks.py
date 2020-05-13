82af903c3545472f272a4a6883561fc6783202c8
pahuja-gor <36912136+pahuja-gor@users.noreply.github.com>
2020-05-13 17:00:55 -0400

    Add 'stocks' project

--- Miscellaneous/stocks.py
+++ Miscellaneous/stocks.py
@@ -1,9 +1,25 @@
+'''
+TODO LIST:
+= buy/sell stocks
+= live monitor
+= check monitor
+'''
+
 from datetime import datetime
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import requests
+
+def color_negative_red(val):
+    """
+    Takes a scalar and returns a string with
+    the css property `'color: red'` for negative
+    strings, black otherwise.
+    """
+    color = 'red' if val < 0 else 'black'
+    return 'color: %s' % color
 
 stocksList = requests.get('https://financialmodelingprep.com/api/v3/company/stock/list').json()
 
@@ -56,14 +72,17 @@ for stock in stocks_symbols:
     #              label=stock)
     stock_count += 1
 
-    plt.plot(list(stock_closing_prices.keys())[0:7], list(stock_closing_prices.values())[0:7], color = np.random.rand(3, ), label=stock)
+    '''plt.plot(list(stock_closing_prices.keys())[0:7], list(stock_closing_prices.values())[0:7], color = np.random.rand(3, ), label=stock)
     plt.title(f'Price of {stock} in USD from {list(stock_closing_prices.keys())[0]} to {list(stock_closing_prices.keys())[6]}')
     plt.xticks(rotation=45)
     plt.legend(loc='center right')
     plt.savefig(f'C:/Users/gorpa/Documents/Stocks/{stock}_stock.png', bbox_inches='tight')
     # plt.show()
+    plt.show(block=False)
+    plt.pause(0.1)
+    plt.close()
 # plt.legend(loc='best')
-# plt.show()
+# plt.show()'''
 
 dataframe_dict = {"Stock": stocks_symbols,
                   f"CP Recording: {list(stock_closing_prices.keys())[0]}": day1Reading,
@@ -74,6 +93,8 @@ dataframe_dict = {"Stock": stocks_symbols,
                   f"Day 6 CP Recording: {list(stock_closing_prices.keys())[5]}": day6Reading,
                   f"Today's CP Recording: {list(stock_closing_prices.keys())[6]}": currentReading}
 dataframe = pd.DataFrame(dataframe_dict)
+
+dataframe = dataframe.style.applymap(color_negative_red)
 
 pd.set_option('display.max_rows', len(stocks_symbols))
 pd.set_option('display.max_columns', 10)
@@ -87,4 +108,5 @@ dataframe.to_excel('C:/Users/gorpa/Documents/Stocks/stocks_data.xlsx', sheet_nam
 print("Finished exporting Pandas DataFrame to an Excel Spreadsheet")
 print('\n')
 
-print(dataframe)
+# print(dataframe)
+dataframe.head(10)
